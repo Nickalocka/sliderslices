2b42040f0a150d73406fb4a533ab9c4ac4b3c5d4
Nick <nickystrife@gmail.com>
2017-08-20 19:21:17 +0100

    mostly working

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -10253,5 +10253,57 @@ return jQuery;
 } );
 
 $(document).ready(function(){
-	console.log("hiya");
+
+
+sliceSlider = function(){
+
+	$(".slide").each(function(){
+		$(this).css("height", Math.round(($(window).height() * 0.8)) + 1);
+	});
+
+	var sliceScrollTimer = null;
+
+	$(".slice-slider-inner").scroll(function () {
+		$(".slice-slider-inner").scrollTop(0);
+
+	    if (sliceScrollTimer) {
+	        clearTimeout(sliceScrollTimer);
+	    }
+
+	    sliceScrollTimer = setTimeout(slide, 150);
+	});
+
+	slide = function(){
+		sliceScrollTimer = null;
+		var slider = $(".slice-slider");
+		var slides = slider.find(".slider");
+
+		var slide = slides.find(".slide");
+		var height = slide.outerHeight();		
+
+		var x = parseInt($(".slider.left").css("top"));
+
+		if (x == 0) {
+			y = (x + height) * -1;
+		} else {
+			y = (x - height);
+		}
+
+		$(".slider.left").animate({
+			top: y
+		});
+
+		$(".slider.right").animate({
+			bottom: y
+		});
+
+		$(".slice-slider-inner").scrollTop(0);
+
+	};
+};
+
+sliceSlider();
+
+
+
 });
